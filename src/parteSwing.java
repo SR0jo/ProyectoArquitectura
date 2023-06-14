080c0f3f074573fbec8c33edc4a097f318e84e09
santi <santiagomza12@gmail.com>
2023-06-14 17:34:57 -0300

    Varias pruebas

---
+++ src/parteSwing.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class parteSwing {
+
+    private JPanel panel;
+    private JLabel label1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
